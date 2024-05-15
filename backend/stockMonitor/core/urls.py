3c7a1a48ad1028eed115ddc3b94b00465ad4458e
Daria2605 <144656784+Daria2605@users.noreply.github.com>
2024-05-15 17:28:42 +0300

    Update urls.py

--- backend/stockMonitor/core/urls.py
+++ backend/stockMonitor/core/urls.py
@@ -5,13 +5,11 @@ from drf_spectacular.views import SpectacularSwaggerView
 
 import authentification.urls
 import core.views
-import portfolio.urls
 import stocks.urls
 
 urlpatterns = [
     path("auth/", include(authentification.urls)),
     path("market/", include(stocks.urls)),
-    path("portfolio/", include(portfolio.urls)),
     path(
         "get_site_name",
         core.views.GetNameView.as_view(),
