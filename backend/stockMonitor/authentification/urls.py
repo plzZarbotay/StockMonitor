cebfd9923e2fd54df83c68078c3a1753b14c1b9e
Daria2605 <144656784+Daria2605@users.noreply.github.com>
2024-05-15 17:23:39 +0300

    Made an openapi docs

--- backend/stockMonitor/authentification/urls.py
+++ backend/stockMonitor/authentification/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from django_rest_passwordreset.views import reset_password_confirm
 from django_rest_passwordreset.views import reset_password_request_token
+from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema_view
+from drf_spectacular.utils import inline_serializer
+from drf_spectacular.utils import OpenApiResponse
+from rest_framework.serializers import CharField
+from rest_framework.serializers import ListField
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -19,7 +25,7 @@ urlpatterns = [
     ),
     path("refresh-token/", TokenRefreshView.as_view(), name="refresh-token"),
     path(
-        "check-existance/",
+        "check_existance/",
         views.CheckExistanceView.as_view(),
         name="check-existance",
     ),
@@ -29,11 +35,64 @@ urlpatterns = [
         name="register",
     ),
     path(
-        "reset_password/", reset_password_request_token, name="reset_password"
+        "reset_password/",
+        extend_schema_view(
+            post=extend_schema(
+                description="Endpoint for creating request"
+                " for password change",
+                responses={
+                    200: OpenApiResponse(
+                        inline_serializer(
+                            name="register_response_ok",
+                            fields={"status": CharField()},
+                        ),
+                        description="Email found. Email sent",
+                    ),
+                    400: OpenApiResponse(
+                        inline_serializer(
+                            name="register_response_error",
+                            fields={"email": ListField()},
+                        ),
+                        description="Error with email. Details in `email`",
+                    ),
+                },
+            )
+        )(reset_password_request_token),
+        name="reset_password",
     ),
     path(
         "reset_password/confirm/",
-        reset_password_confirm,
+        extend_schema_view(
+            post=extend_schema(
+                description="Endpoint for confirming password change."
+                " Field `password` is a new password",
+                responses={
+                    200: OpenApiResponse(
+                        inline_serializer(
+                            name="change_password_ok",
+                            fields={"status": CharField()},
+                        ),
+                        description="Password changed",
+                    ),
+                    404: OpenApiResponse(
+                        inline_serializer(
+                            name="change_password_error_404",
+                            fields={"detail": CharField()},
+                        ),
+                        description="Password not changed,"
+                        " maybe token expired",
+                    ),
+                    400: OpenApiResponse(
+                        inline_serializer(
+                            name="change_password_error_400",
+                            fields={"password": ListField()},
+                        ),
+                        description="Password not changed,"
+                        " password maybe small or something like that",
+                    ),
+                },
+            )
+        )(reset_password_confirm),
         name="reset_password_confirm",
     ),
 ]
--- backend/stockMonitor/core/urls.py
+++ backend/stockMonitor/core/urls.py
@@ -1,16 +1,26 @@
 from django.urls import include
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularSwaggerView
 
 import authentification.urls
 import core.views
+import portfolio.urls
 import stocks.urls
 
 urlpatterns = [
     path("auth/", include(authentification.urls)),
     path("market/", include(stocks.urls)),
+    path("portfolio/", include(portfolio.urls)),
     path(
         "get_site_name",
         core.views.GetNameView.as_view(),
         name="get_site_name",
+    ),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="schema-ui",
     ),
 ]
