2f839adb8e5b9dc0b0b841eaf4fdfac2928bc14a
Daria Zhgenti <144656784+Daria2605@users.noreply.github.com>
2024-05-22 18:37:06 +0300

    Create search.vue

---
+++ frontend/homepage/search.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="top_rectangle"> </div>
+    <input type="stroke" class="search_text" placeholder="Поиск инструментов">
+    <p>
+        <span class="logo1"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\logo1.png"></span>
+        <span class="search_icon"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\search_icon.png"></span>
+        <span class="market">Рынок</span>
+        <span class="analytics">Аналитика</span>
+        <span class="to_come_in" style="color: #54818F;">Войти</span>
+        <span class="top_grad_sq"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\top_grad_sq.png"</span>
+
+    </p>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+input {
+    outline: none;
+    width: 40%;
+    height: 30px;
+    left: 30%;
+    position: relative;
+    display: flex;
+    top: -100px;
+    font-size: 1.8 em;
+    font-weight: 520;
+    font-family: system-ui;
+    text-align: center;
+    border-radius: 7px;
+    border: 0;
+    padding: 10 px 5 px;
+    background: rgb(11, 50, 73);
+    color: rgba(84, 129, 143, 1);
+}
+</style>
