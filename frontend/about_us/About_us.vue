36faf9daaaee0c91738dfdfe114747ff21041656
Daria Zhgenti <144656784+Daria2605@users.noreply.github.com>
2024-05-24 16:34:52 +0300

    Create About_us.vue

---
+++ frontend/about_us/About_us.vue
@@ -0,0 +1,17 @@
+<template>
+    <span class="rec_about_us"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\About us.svg"></span>
+    <span class="top_rec"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\top_rec.svg" weigh="80%" height="80%"></span>
+    <span class="sr_rec"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\sr_rec.svg" weigh="80%" height="80%"></span>
+    <span class="bot_rec"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\bot_rec.svg" weigh="80%" height="80%"></span>
+    <span class="text_und_tit_text"><b>SMM</b> - это веб-интерфейс, где парсятся данные, строятся красивые отчеты и производится анализ фондового рынка в целом: объем продаж, тренды покупателей, динамика изо дня в день.</span>
+    <span class="join"><img src="C:\Users\zhgen\Desktop\2-ой семестр\Проект\project\src\components\icons\join.svg"></span>
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
+
+
